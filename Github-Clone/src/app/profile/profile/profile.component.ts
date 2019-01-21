1456410adeef5b5da1e2bdb8e4c5dab63de1f580
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-01-21 16:14:15 +0300

    add injection

--- Github-Clone/src/app/profile/profile/profile.component.ts
+++ Github-Clone/src/app/profile/profile/profile.component.ts
@@ -1,39 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from './../profile.service';
-import { Component, OnInit, Input } from '@angular/core';
+import {  ProfileService } from './../profile.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Users } from '../users';
-import { Repo } from '../repo';
+import { Repository } from '../repository';
 import { getResponseURL } from '@angular/http/src/http_utils';
 import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
   providers: [ProfileService]
+
 })
 export class ProfileComponent implements OnInit {
+user: Users;
+repos: Repo;
+userRepos: any;
+userName: string;
 
-  constructor(private userService: UserService) { }
+  constructor(private profileService: ProfileService) { }
   findProfile() {
 
-   this.userService.updateProfile(this.userName);
+   this.profileService.updateProfile(this.userName);
 
-   this.userService.getUserInfo();
-   this.user = this.userService.user;
+   this.profileService.getUserInfo();
+   this.user = this.profileService.user;
   console.log(this.user);
 
-   this.userService.getRepoInfo(this.userName);
-   this.userRepos = this.userService.newRepo;
+   this.profileService.getRepoInfo(this.userName);
+   this.userRepos = this.profileService.newRepo;
  }
 
+
   ngOnInit() {
-  this.userService.getRepoInfo(this.userName);
-    this.repos = this.userService.repo;
-    this.userService.getUserInfo();
-    this.user = this.userService.user;
+  this.profileService.getRepoInfo(this.userName);
+    this.repos = this.profileService.repo;
+    this.profileService.getUserInfo();
+    this.user = this.profileService.user;
   }
 
 }
