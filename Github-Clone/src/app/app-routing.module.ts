a62854ad4f05f7361301aa831f200186e47ca893
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-01-21 17:17:02 +0300

    delete landing page component

--- Github-Clone/src/app/app-routing.module.ts
+++ Github-Clone/src/app/app-routing.module.ts
@@ -1,27 +1,10 @@
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {RouterModule, Routes} from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-import { UserComponent } from './user/user.component';
-
-import { ProfileComponent } from '../profile/profile.component';
-
-// Defining routes
-const routes: Routes = [
-  {path: '', component: LandingPageComponent},
- {path: 'user/repositories?', component: ProfileComponent},
- {path: 'userprofile?', component: UserComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
- imports: [
-   CommonModule,
-   RouterModule.forRoot(routes)
-
- ],
- exports: [RouterModule],
- declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class RoutingModule { }
+export class AppRoutingModule { }
