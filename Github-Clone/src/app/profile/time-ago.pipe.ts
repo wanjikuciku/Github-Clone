22603a9b79124a5a34aec014385f88774945fe38
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-01-21 16:20:06 +0300

    add functionality to the pipe

--- Github-Clone/src/app/profile/time-ago.pipe.ts
+++ Github-Clone/src/app/profile/time-ago.pipe.ts
@@ -5,8 +5,47 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeAgoPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+now: Date;
+seconds: number;
+minutes: number;
+hours: number;
+days: number;
+months: number;
+transform(posted: Date): any {
+  this.now=new Date();
+  this.seconds=Math.floor(this.now.getTime()/1000-posted.getTime()/1000);
+  this.minutes=Math.floor(this.seconds/60);
+  this.hours=Math.floor(this.minutes/60);
+  this.days=Math.floor(this.hours/24);
+  this.months=Math.floor(this.days/30);
+  if (this.months > 0) {
+    if (this.months == 1) {
+      return this.months + ' month ago'
+    }
+    return this.months + ' months ago'
   }
+  if (this.days > 0) {
+    if (this.days == 1) {
+      return this.days + ' day ago'
+    }
+    return this.days + ' days ago'
+  }
+  if (this.hours > 0) {
+    if (this.hours == 1) {
+      return this.hours + ' hour ago'
+    }
+    return this.hours + ' hours ago'
+  }
+  if (this.minutes > 0) {
+    if (this.minutes == 1) {
+      return this.minutes + ' minute ago'
+    }
+    return this.minutes + ' minutes ago'
+  }
+  if(this.seconds==1){
+    return this.seconds+' second ago'
+  }
+  return this.seconds + ' seconds ago'
+}
 
 }
